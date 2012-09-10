061d97e4a1bb1e020b29ef5c5cf983dc5288284d
ederFortunato <eder.fortunato@gmail.com>
2012-09-10 15:10:59 -0300

    First Commit

---
+++ app/crud.js
@@ -0,0 +1,165 @@
+define([
+  // Global application context.
+  "app",
+
+  // Third-party libraries.
+  "backbone",
+
+  "plugins/backbone.localstorage",
+  "plugins/bootstrap-dropdown",
+  "plugins/bootstrap-modal"
+],
+function (app, Backbone) {
+  "use strict";
+
+  var Crud = app.module();
+
+  //------------------------------------------------
+  // MODEL
+  //------------------------------------------------
+
+  var MovieModel = Backbone.Model.extend({
+    defaults: {
+      cod: '',
+      title: '',
+      status: '0',
+      rating: ''
+    }
+  });
+
+  //------------------------------------------------
+  // COLLECTION
+  //------------------------------------------------
+
+  Crud.Collection = Backbone.Collection.extend({
+    model: MovieModel,
+    localStorage: new Store('movie-list')
+  });
+
+
+  //------------------------------------------------
+  // VIEW'S
+  //------------------------------------------------
+
+  Crud.Views.Form = Backbone.View.extend({
+    template: "form",
+    currentItem: {},
+
+    initialize: function () {
+      this.currentItem =  new MovieModel();
+
+      this.collection.on('edit', this.editMovie, this);
+    },
+
+    events: {
+      'click #btnConfirm' : 'confirmSave',
+      'click #btn-add' : 'openForm',
+      'click .btnClose' : 'closeForm'
+    },
+
+    openForm: function () {
+      $('#fooom').modal('show');
+    },
+
+    closeForm: function () {
+      app.router.navigate('/');
+      this.currentItem =  new MovieModel();
+      this.render();
+    },
+
+    confirmSave: function () {
+      app.router.navigate('/');
+
+      this.currentItem.set({
+        title: $('#txtTitle').val()
+      });
+
+      if (!this.currentItem.get('cod')) {
+        this.currentItem.set({ cod: (new Date()).getTime() });
+        this.collection.add(this.currentItem);
+      }
+
+      this.currentItem.save();
+
+      this.currentItem = new MovieModel();
+
+      this.render();
+    },
+
+    serialize: function () {
+      return this.currentItem.toJSON();
+    },
+
+    editMovie: function (movie) {
+      this.currentItem = movie;
+      this.render();
+      this.openForm();
+    }
+
+  });
+
+  Crud.Views.List = Backbone.View.extend({
+    template: "list",
+
+    initialize: function () {
+
+      this.collection.on('reset', function () {
+        this.collection.each(this.addMovie, this);
+      }, this);
+
+      this.collection.on("add", function (movie) {
+        this.addMovie(movie);
+      }, this);
+
+    },
+
+    addMovie: function (movie) {
+      var vList = this.insertView('tbody', new Crud.Views.Item({model: movie}));
+      vList.render();
+    }
+  });
+
+  Crud.Views.Item = Backbone.View.extend({
+    template: 'item',
+    tagName: "tr",
+
+    initialize: function () {
+      this.model.on('change', function () {
+        this.render();
+      }, this);
+
+      this.model.on('destroy', function () {
+        this.remove();
+      }, this);
+    },
+    events: {
+      'click #btnStatusNotWatched' : 'changeStatus',
+      'click #btnStatusWatched' : 'changeStatus',
+      'click #btnStatusWantWatched' : 'changeStatus',
+      'click .rating span' : 'changeRating'
+    },
+    changeStatus: function (e) {
+      this.model.save({status: $(e.currentTarget).data('status')});
+      this.render();
+    },
+    changeRating: function (e) {
+      var r = 5 - $(e.currentTarget).index();
+      this.model.save({rating: r});
+      this.render();
+    },
+    serialize: function () {
+      return {
+        model: this.model.toJSON()
+      };
+    },
+    afterRender: function() {
+      var csStatus = ['', 'btn-success', 'btn-warning'];
+      this.$el.find('.dropdown-toggle').addClass(csStatus[this.model.get('status')]);
+      this.$el.find('.star:eq('+(5-this.model.get('rating'))+')').addClass('starMark');
+    }
+
+  });
+
+  return Crud;
+
+});
---
+++ app/router.js
@@ -0,0 +1,68 @@
+define([
+  // Application.
+  "app",
+
+  "crud",
+
+  "backbone"
+],
+
+function (app, Crud, Backbone) {
+  "use strict";
+
+  // Defining the application router, you can attach sub routers here.
+  var Router = Backbone.Router.extend({
+    routes: {
+      "": "index",
+      "edit/:id": "editMovie",
+      "delete/:id": "deleteMovie"
+    },
+
+    index: function () {
+
+    },
+
+    initialize: function () {
+
+      this._collection  = new Crud.Collection();
+
+      // Use main layout and set Views.
+      app.useLayout("main");
+
+      app.layout.setViews({
+        "#list-container": new Crud.Views.List({
+          'id': 'list-view',
+          collection: this._collection
+        }),
+        "#form-container": new Crud.Views.Form({
+          'id': 'form-view',
+          collection: this._collection
+        })
+      });
+
+      // Use the main layout.
+      app.useLayout("main").render();
+
+      this._collection.fetch();
+
+    },
+
+    editMovie: function (id) {
+      var models = this._collection.where({cod: Number(id)});
+      if (models.length > 0) {
+        models[0].trigger('edit', models[0]);
+      }
+    },
+
+    deleteMovie: function (id) {
+      var models = this._collection.where({cod: Number(id)});
+      if (models.length > 0) {
+        models[0].destroy();
+      }
+      this.navigate('/');
+    }
+  });
+
+  return Router;
+
+});
